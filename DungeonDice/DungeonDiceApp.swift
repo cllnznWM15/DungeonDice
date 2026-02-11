27e1a63e99fea2fe2ed2da90082935cc3ecbf2ae
Nick Collins <necollins03@gmail.com>
2026-02-11 13:28:37 -0500

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Nick Collins on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
