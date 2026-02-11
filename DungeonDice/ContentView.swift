639a6798977a46049494e2b07e7edae7d9a4ee49
Nick Collins <necollins03@gmail.com>
2026-02-11 14:00:37 -0500

    DungeonDice UI

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "Roll a die!"
+    private let diceTypes = [4, 6, 8, 10, 12, 20, 100]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dungeon Dice!")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.red)
+            Spacer()
+            
+            Text(message)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            Group {
+                ForEach(diceTypes, id: \.self) { diceType in
+                    Button("\(diceType)-sided") {
+                        rollDie(sides: diceType)
+                    }
+                }
+            }
+            .buttonStyle(.glassProminent)
+            .font(.title2)
+            .tint(.red)
         }
         .padding()
+        
+        }
+    func rollDie(sides:Int) {
+        let result = Int.random(in: 1...sides)
+        message = "You rolled a \(result) on a \(sides)-sided die"
+
     }
 }
-
-#Preview {
-    ContentView()
-}
+    
+    #Preview {
+        ContentView()
+    }
