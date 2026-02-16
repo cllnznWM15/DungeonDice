1dd1ecb6dd841c69a4c67d3146de4d5a28df490e
Nick Collins <necollins03@gmail.com>
2026-02-16 16:38:28 -0500

    Update from Feb 16

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,26 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Dice: Int, CaseIterable, Identifiable {
+        case d4 = 4, d6 = 6, d8 = 8, d10 = 10, d12 = 12, d20 = 20, d100 = 100
+        
+        var id: Int {self.rawValue}
+        
+        var dieName: String {
+            return "\(self)".capitalized
+        }
+        
+        var roll: Int {
+            return Int.random(in: 1...self.rawValue)
+        }
+    }
     @State private var message = "Roll a die!"
-    private let diceTypes = [4, 6, 8, 10, 12, 20, 100]
+    @State private var animationTrigger = false
+    @State private var isDoneAnimating = true
+    @State private var rolls: [Int] = []
+    private var grandTotal: Int {rolls.reduce(0, +)}
     var body: some View {
         VStack {
             Text("Dungeon Dice!")
-                .font(.largeTitle)
+                .font(.title)
                 .fontWeight(.black)
                 .foregroundStyle(.red)
+            
+            GroupBox {
+                ForEach(rolls, id: \.self) { roll in
+                    Text("\(roll)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Divider()
+                }
+                                
+                HStack{
+                    Text("TOTAL: \(rolls.reduce(0, +))")
+                        .font(.title2)
+                        .bold()
+                        .monospacedDigit()
+                        .contentTransition(.numericText())
+                        .animation(.default, value: grandTotal)
+                    Spacer()
+                    Button("Clear") {
+                        rolls.removeAll()
+                    }
+                    .buttonStyle(.glass)
+                    .tint(.red)
+                    .disabled(rolls.isEmpty)
+                }
+            } label: {
+                Text("Session Rolls:")
+                    .font(.title2)
+                    .bold()
+            }
+
             Spacer()
             
             Text(message)
                 .font(.largeTitle)
                 .multilineTextAlignment(.center)
+//                .scaleEffect(isDoneAnimating ? 1.0 : 0.6) // animate to 1.0 until isdone stops
+//                .opacity(isDoneAnimating ? 1.0 : 0.2)
+                .rotation3DEffect(isDoneAnimating ? .degrees(360) : .degrees(0), axis: (1,0,0))
+                .onChange(of: animationTrigger) {
+                    isDoneAnimating = false
+                    withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)) {
+                        isDoneAnimating = true
+                    }
+                }
             
             Spacer()
             
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 110))]) {
-                ForEach(diceTypes, id: \.self) { diceType in
-                    Button("\(diceType)-sided") {
-                        rollDie(sides: diceType)
+                ForEach(Dice.allCases) { die in
+                    Button("\(die.rawValue)-sided") {
+                        animationTrigger.toggle()
+                        let roll = die.roll
+                        message = "You rolled a \(roll) on a \(die)."
+                        rolls.append(roll)
                     }
                     .font(.title2)
                     .lineLimit(1)
@@ -42,11 +99,6 @@ struct ContentView: View {
         .padding()
         
         }
-    func rollDie(sides:Int) {
-        let result = Int.random(in: 1...sides)
-        message = "You rolled a \(result) on a \(sides)-sided die"
-
-    }
 }
     
     #Preview {
